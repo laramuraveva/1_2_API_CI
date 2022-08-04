82fa667f6cef05c22d1af7340fcd1ee0230e409a
laramuraveva <lara.chernishova@yandex.ru>
2022-08-04 16:46:23 +0300

    Test RUB/USD added

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
 
 
 class MobileBankApiTestV1 {
@@ -20,7 +21,27 @@ class MobileBankApiTestV1 {
       .then()
           .statusCode(200)
         .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+    }
 
+    @Test
+    void shouldCurrencyRub() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .body("[0].currency", equalTo("RUB"));
+
+    }
+
+    @Test
+    void shouldCurrencyUsd() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .body("[0].currency", equalTo("USD"));
 
     }
 }
